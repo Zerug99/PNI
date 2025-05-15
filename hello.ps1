feab523cf7ea16f9389da5b4a46051e98d42da26
unknown <daud357@outlook.com>
2025-05-15 14:33:23 +0200

    konflikt

--- hello.ps1
+++ hello.ps1
@@ -2,7 +2,7 @@
 Write-Host "hej från $whoami" 
 #Hej Imer 
 =======
-Write-Host "hej från $whoami" hejhej
+Write-Host "hej från $whoami" hejhej 12345
 # HELLO woRLD!
 # Ouch
 >>>>>>> 15351c5e0c86c065667d2bd3aa377e711f05ebb2
