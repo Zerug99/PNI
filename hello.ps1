2f2cfc35c0efb9204c7e7491a09d213397918210
unknown <patryk-99@hotmail.com>
2025-05-15 14:55:03 +0200

    den nya versionen

--- hello.ps1
+++ hello.ps1
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+ HEAD
 Write-Host "hej från $whoami" 
 #Hej Imer 
 =======
-<<<<<<< HEAD
+HEAD
 Write-Host "hej från $whoami" hejhej 12345 ty
 =======
 >>>>>>> e9d2389fb47ec3a04f548cba7dd5eade7d157068
 # HELLO woRLD!
 # Ouch
->>>>>>> 15351c5e0c86c065667d2bd3aa377e711f05ebb2
+ 15351c5e0c86c065667d2bd3aa377e711f05ebb2
